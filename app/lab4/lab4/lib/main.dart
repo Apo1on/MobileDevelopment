5023f40d97753ccb3e5ae1c7f7e0892fa931c5dd
Огузок (Афанасьев Евгений) <evganev@mail.ru>
2025-04-29 12:31:19 +0900

    Update main.dart

--- app/lab4/lab4/lib/main.dart
+++ app/lab4/lab4/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.indigo,
       ),
       home: const IMTCal(),
-      routes: AppRoutes.routes, // Add the routes
+      routes: AppRoutes.routes,
     );
   }
 }
